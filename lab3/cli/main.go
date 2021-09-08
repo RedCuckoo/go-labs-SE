a994f87d26706433a99cc88b88c5adcc136bcd46
Yurii Momotenko <momotenko.yuriy@gmail.com>
2021-09-09 01:49:04 +0300

    Added run service command

--- lab3/cli/main.go
+++ lab3/cli/main.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"github.com/alecthomas/kingpin"
 	"github.com/redcuckoo/go-labs-SE/lab3/config"
+	"github.com/redcuckoo/go-labs-SE/lab3/service"
 	"log"
 )
 
@@ -25,6 +26,9 @@ func Run(args []string) bool{
 	migrateUpCmd := migrateCmd.Command("up", "migrate database up")
 	migrateDownCmd := migrateCmd.Command("down", "migrate database down")
 
+	runCmd := app.Command("run", "run command")
+	serviceCmd := runCmd.Command("service", "run service")
+
 	cmd, err := app.Parse(args[1:])
 	if err != nil {
 		log.Fatalf("failed to parse arguments, err: %v", err)
@@ -36,6 +40,9 @@ func Run(args []string) bool{
 		err = MigrateUp(configure)
 	case migrateDownCmd.FullCommand():
 		err = MigrateDown(configure)
+	case serviceCmd.FullCommand():
+		service.Main()
+		return true
 	default:
 		log.Fatalf("unknown command %v", cmd)
 		return false
--- lab3/main.go
+++ lab3/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"github.com/redcuckoo/go-labs-SE/lab3/cli"
-	"github.com/redcuckoo/go-labs-SE/lab3/service"
 	"os"
 )
 
 func main() {
-	service.Main()
-
 	if !cli.Run(os.Args) {
 		os.Exit(1)
 	}
